Exercise 4 with incorrect ridge regression and correct bias-variance decomposition

Labs/ex04/template/build_polynomial.py
@@ -6,10 +6,13 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # polynomial basis function: TODO
+        # this function should return the matrix formed
+        # by applying the polynomial basis to the input data
+        # ***************************************************
+        phi = np.zeros((x.shape[0], degree+1))
+        for i in range(0, degree+1):
+            phi[:,i] = np.power(x,i)
+        return phi