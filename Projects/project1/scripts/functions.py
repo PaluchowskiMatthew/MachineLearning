07c60503d4fa06483881a7e4c438159f15f012ee
Laz <lazare.girardin@epfl.ch>
2016-10-26 19:02:47 +0200

    26 october

--- Projects/project1/scripts/functions.py
+++ Projects/project1/scripts/functions.py
@@ -15,11 +15,16 @@ def build_poly(x, degree):
     
     return phi
 
-def compute_loss(y, tx, w):
+#def compute_loss(y, tx, w):
     """Calculate the loss."""
+ #   e = y - np.dot(tx, w)
+ #   return e
 
+def compute_RMSE(y, tx, w):
     e = y - np.dot(tx, w)
-    return e
+    mse = np.dot(np.transpose(e), e)/(2*len(y))
+    rmse = np.sqrt(2*mse)
+    return rmse
 
 def least_squares(y, tx):
     """calculate the least squares solution."""
@@ -31,9 +36,6 @@ def least_squares(y, tx):
 
 def ridge_regression(y, tx, lamb):
     """implement ridge regression."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
     tx_transposed = np.transpose(tx)
     lambda2 = lamb*2*len(y)
     txxt = np.dot(tx_transposed, tx)
@@ -53,7 +55,6 @@ def standardize(x, mean_x=None, std_x=None):
     
     tx = np.hstack((np.ones((x.shape[0],1)), x))
     return tx, mean_x, std_x
-
 
 def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
     """
@@ -146,8 +147,14 @@ def calculate_hessian(y, tx, w):
 
 def sigmoid(t):
     """apply sigmoid function on t."""
-    e_t = np.exp(t)
-    sig = e_t/(1+e_t)
+    sig = np.zeros((t.shape[0], 1))
+    for i in range(len(t)):
+        if t[i]>100:    
+            sig[i] = 0.99999
+        elif t[i] < 100:
+            sig[i] = 0.000001
+        else:
+            sig[i] = 1/(1+np.exp(-t[i]))
     return sig
 
 def calculate_loss(y, tx, w):
@@ -166,25 +173,71 @@ def learning_by_gradient_descent(y, tx, w, alpha):
     Do one step of gradient descen using logistic regression.
     Return the loss and the updated w.
     """
-    loss = calculate_loss(y, tx, w)
+    mse = compute_loss(y, tx, w)
+    rmse = np.sqrt(2*mse)
     L_grad = calculate_gradient(y, tx, w)
     w = w - np.dot(alpha, L_grad)
-    return loss, w
+    return rmse, w
 
 def logistic_regression(y, tx, w):
     """return the loss, gradient, and hessian."""
-    loss = calculate_loss(y, tx, w)
+    #loss = calculate_loss(y, tx, w)
+    mse = compute_loss(y, tx, w)
+    rmse = np.sqrt(2*mse)
     grad = calculate_gradient(y, tx, w)
     hess = calculate_hessian(y, tx, w)
-    return loss, grad, hess
+    return rmse, grad, hess
 
 def learning_by_newton_method(y, tx, w, alpha):
     """
     Do one step on Newton's method.
     return the loss and updated w.
     """
-    loss, grad, hess = logistic_regression(y, tx, w)
+    rmse, grad, hess = logistic_regression(y, tx, w)
     w = w - np.dot(alpha, np.linalg.solve(hess, grad))
+    return rmse, w
+
+def IRLS(y, tx, w):
+    sig = sigmoid(np.dot(tx, w))
+    S_diag = np.multiply(sig, 1-sig)
+    S = np.diagflat(S_diag)
+    z = np.dot(tx, w)+np.linalg.solve(S, y-sig)
+    loss = calculate_loss(y, tx, w)
+    return S, z, loss
+
+def learning_by_IRLS(y, tx, w):
+    S, z, loss = IRLS(y, tx, w)
+    txt_S = np.dot(np.transpose(tx), S)
+    w = np.linalg.solve(np.dot(txt_S, tx),np.dot(txt_S, z))
+    return loss, w
+
+def penalized_logistic_regression(y, tx, w, lambd):
+    """return the loss, gradient, and hessian."""
+    # ***************************************************
+    # return loss, gradient, and hessian
+    #loss = calculate_loss_by_likelyhood(y, tx, w)
+    gradient = calculate_gradient(y, tx, w)
+    hessian = calculate_hessian(y, tx, w)
+    #loss_penalty = lambd * np.sum(np.power(w, 2))
+    gradient_penalty = lambd * 2 * w / np.linalg.norm(w)
+    return gradient + gradient_penalty, hessian
+ 
+def reg_logistic_regression(y, tx, lambd , gamma, max_iters):
+    """
+   Penalized logistic regression using gradient descent.
+   Return the loss and the updated w.
+   """
+    # start the logistic regression
+    w = np.zeros((tx.shape[1],1))
+    for iter in range(max_iters):
+        # get loss and update w.
+        gradient, hessian = penalized_logistic_regression(y, tx, w, lambd)
+        # ***************************************************
+        # update w
+        w = w - gamma * np.linalg.solve(hessian, gradient)
+        # log info
+    loss_penalty = lambd * np.sum(np.power(w, 2))
+    loss = compute_RMSE(y, tx, w) + loss_penalty
     return loss, w
 
 def split_data(x, y, ratio, seed=1):
@@ -197,70 +250,60 @@ def split_data(x, y, ratio, seed=1):
     np.random.seed(seed)
     shuffled_y = np.random.permutation(y)
     
-    test_x_size = int(shuffled_x.shape[0] * ratio)
-    training_x = shuffled_x[0:test_x_size]
-    test_x = shuffled_x[-test_x_size:]
+    train_x_size = int(shuffled_x.shape[0] * ratio)
+    training_x = shuffled_x[:train_x_size]
+    test_x = shuffled_x[(train_x_size):]
     
-    test_y_size = int(shuffled_y.shape[0] * ratio)
-    training_y = shuffled_y[0:test_y_size]
-    test_y = shuffled_y[-test_y_size:]
+    train_y_size = int(shuffled_y.shape[0] * ratio)
+    training_y = shuffled_y[:train_y_size]
+    test_y = shuffled_y[(train_y_size):]
 
     return training_x, test_x, training_y, test_y
 
-""" *************  MATEUSZ FUNCTIONS *********************************************
-def compute_gradient(y, tx, w):
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
 
-    N = y.shape[0]
-    yy = np.array([y]).T
-    normalization = -1/N
-    der_w0 = normalization * np.sum((yy - np.dot(tx, w)))
-    der_w1 = normalization * np.dot(np.array([tx[:,1]]), yy - np.dot(tx, w) )[0][0]
+def cross_validation(y, x, k_fold, seed, lambd, max_iter):
+    """return the loss of ridge regression."""
+    loss_tr, loss_te = 0.0, 0.0
+    k_indices = build_k_indices(y, k_fold, seed)
+    max_iters=1000
+    weights = np.zeros((k_fold, x.shape[1]+1))
+    for fold in range(0, k_fold):
+        # ***************************************************
+        # get k'th subgroup in test, others in train
+        # ***************************************************
+        y_test, y_train, x_test, x_train = np.array([]), np.array([]), np.empty((0,x.shape[1]), float), np.empty((0,x.shape[1]), float)
+        x_train = x[k_indices[fold, :]]
+        y_train = y[k_indices[fold, :]]
 
-    gradient = np.array([[der_w0], [der_w1]])
-    return gradient
-# compute_gradient(np.array([100, 50]), np.array([[1, 10], [1, 10]]), np.array([[1],[0.1]]))
+        x_test = np.delete(x, k_indices[fold, :], axis=0)
+        y_test = np.delete(y, k_indices[fold, :], axis=0)
 
-def least_squares_GD(y, tx, initial_w, max_iters, gamma): 
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        gradient = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w)
-        w = w - (gamma * gradient)
-        ws.append(np.copy(w))
-        losses.append(loss)
+        tx_train, x_tr_mean, x_tr_std = standardize(x_train)
+        tx_test, x_te_mean, x_te_std = standardize(x_test)
+        w = np.zeros((tx_train.shape[1], 1))
 
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        for iter in range(max_iter):
+            train_rmse, w = learning_by_newton_method(y_train, tx_train, w, lambd)
+                                    #ridge_regression(y_train, x_train, lambd)
+                                    #least_squares_GD(y_train, x_train, lambd, max_iters)
+                                    #ridge_regression(y_train, x_train, lambd)
+        # ***************************************************
+        # calculate the loss for train and test data
+        # ***************************************************
+        test_mse = compute_loss(y_test, tx_test, w)
+        train_mse = compute_loss(y_train, tx_train, w)
+        loss_tr += np.sqrt(2*train_mse)
+        loss_te += np.sqrt(2*test_mse)
+        w = np.reshape(w, tx_train.shape[1])
+        weights[fold] = w
 
-    return losses, ws
-# least_squares_GD(np.array([100, 50]), np.array([[1, 10],[1, 10]]), np.array([[1], [0.1]]), 1, 1)
-    
-    
-def compute_stoch_gradient(y, tx, w, batch_size):
-    
-    gradient_sum = np.array([[0.0],[0.0]])
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
-        grad = compute_gradient(minibatch_y, minibatch_tx, w)
-        gradient_sum += grad
-    return (1/batch_size) * grad
-# grad = compute_stoch_gradient(y, tx, np.array([[0.0], [0.0]]), 1)
-  
-def least_squares_SGD(
-        y, tx, initial_w, batch_size, max_iters, gamma):
-    
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        gradient = compute_stoch_gradient(y, tx, w, batch_size)
-        loss = compute_loss(y, tx, w)
-        w = w - (gamma * gradient)
-        ws.append(np.copy(w))
-        losses.append(loss)
-        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-    return losses, ws
-"""
+    return weights, loss_tr/k_fold, loss_te/k_fold
