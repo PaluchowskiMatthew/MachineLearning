48e148fb3d030f63f7647c8b153de1b55fe7b2f2
Marie Drieghe <marie.drieghe@hotmail.com>
2016-12-01 11:09:42 +0100

    61% for neigh, start of neural method

--- Projects/project2/project_road_segmentation/features.py
+++ Projects/project2/project_road_segmentation/features.py
@@ -3,7 +3,7 @@ def extract_features_edge(img):
     from skimage import feature, color
     feat_m = np.mean(img, axis=(0,1))
     feat_v = np.var(img, axis=(0,1))
-    feat_e = np.asarray(feature.canny(color.rgb2gray(img), sigma=5).sum()).reshape(1,)
+    feat_e = np.asarray(feature.canny(color.rgb2gray(img), sigma=5, high_threshold=0.15, low_threshold=0).sum()).reshape(1,)
     feat = np.append(feat_m, feat_v)
     feat = np.hstack([feat, feat_e])
     return feat
