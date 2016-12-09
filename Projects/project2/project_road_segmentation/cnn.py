22676ddb86dfbfc92ee7fbc49bc1ed916afc3b71
Laz <lazare.girardin@epfl.ch>
2016-12-09 19:59:25 +0100

    first implementation of cnn

---
+++ Projects/project2/project_road_segmentation/cnn.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+
+from image_handling import *
+
+NUMBER_IMGS = 20
+TRAIN_RATIO = 0.4
+IMG_PATCH_SIZE = 16
+NUM_CHANNELS = 3
+
+batch_size = 128
+nb_classes = 2
+nb_epoch = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+pool_size = (2, 2)
+# convolution kernel size
+kernel_size = (3, 3)
+
+input_shape = (IMG_PATCH_SIZE, IMG_PATCH_SIZE, NUM_CHANNELS)
+
+def main():
+
+	# ***************** HANDLE THE DATA **********************************
+	data_dir = 'training/'
+	train_data_filename = data_dir + 'images/'
+	train_labels_filename = data_dir + 'groundtruth/' 
+
+	# Extract data into numpy arrays.
+	data = extract_data(train_data_filename, NUMBER_IMGS, IMG_PATCH_SIZE)
+	labels = extract_labels(train_labels_filename, NUMBER_IMGS, IMG_PATCH_SIZE)
+	#print(train_data.shape)
+	#print(train_labels.shape)
+
+	# Create train and test sets
+	idx = np.random.permutation(np.arange(data.shape[0]))
+	train_size = int(TRAIN_RATIO*data.shape[0])
+	X_train = data[idx[:train_size]]
+	Y_train = labels[idx[:train_size]]
+	X_test = data[idx[train_size:]]
+	Y_test = labels[idx[train_size:]]
+
+	# **************** DEFINE THE MODEL ARCHITECTURE *******************
+	model = Sequential()
+
+	# Convolution layer with rectified linear activation
+	model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+							border_mode='same',
+							input_shape=input_shape))
+	model.add(Activation('relu'))
+
+	# Second convolution
+	model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+	model.add(Activation('relu'))
+
+	# Pooling and dropout
+	model.add(MaxPooling2D(pool_size=pool_size))
+	model.add(Dropout(0.25))
+
+	# Full-connected layer
+	model.add(Flatten())
+	model.add(Dense(128))
+	model.add(Activation('relu'))
+
+	# Dropout to avoid overfitting
+	model.add(Dropout(0.5))
+
+	#Fully-connected layer to ouptut the resulting class
+	model.add(Dense(nb_classes))
+	model.add(Activation('softmax'))
+
+	model.compile(loss='categorical_crossentropy',
+	          optimizer='adadelta',
+	          metrics=['accuracy'])
+
+	model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          	   verbose=1, validation_data=(X_test, Y_test))
+
+	score = model.evaluate(X_test, Y_test, verbose=0)
+	print('Test score:', score[0])
+	print('Test accuracy:', score[1])
---
+++ Projects/project2/project_road_segmentation/image_handling.py
@@ -0,0 +1,78 @@
+import gzip
+import os
+import sys
+import urllib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+
+# Extract patches from a given image
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+def extract_data(filename, num_images, IMG_PATCH_SIZE):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    imgs = []
+    for i in range(1, num_images+1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print ('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            imgs.append(img)
+        else:
+            print ('File ' + image_filename + ' does not exist')
+
+    num_images = len(imgs)
+    IMG_WIDTH = imgs[0].shape[0]
+    IMG_HEIGHT = imgs[0].shape[1]
+    N_PATCHES_PER_IMAGE = (IMG_WIDTH/IMG_PATCH_SIZE)*(IMG_HEIGHT/IMG_PATCH_SIZE)
+
+    img_patches = np.asarray([img_crop(imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)])
+    data = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
+
+    return np.asarray(data)
+
+def value_to_class(v):
+    foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+    df = np.sum(v)
+    if df > foreground_threshold:
+        return [1, 0] #              *****  category matrix
+    else:
+        return [0, 1]
+
+def extract_labels(filename, num_images, IMG_PATCH_SIZE):
+    """Extract the labels into a 1-hot matrix [image index, label index]."""
+    gt_imgs = []
+    for i in range(1, num_images+1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print ('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            gt_imgs.append(img)
+        else:
+            print ('File ' + image_filename + ' does not exist')
+
+    num_images = len(gt_imgs)
+    gt_patches = [img_crop(gt_imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
+    data = np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+    labels = np.asarray([value_to_class(np.mean(data[i])) for i in range(len(data))])
+
+    # Convert to dense 1-hot representation.
+    return labels.astype(np.float32)
